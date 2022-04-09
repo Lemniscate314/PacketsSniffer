3ace07cf5028dc0b450ae62a608c8a181c8ee36d
issa <Issa Mboup>
2022-04-09 13:03:58 -0400

    Find a device

--- PacketSniffer.c
+++ PacketSniffer.c
@@ -7,7 +7,7 @@ int main (int args, char **argv)
     char error_lookup [PCAP_ERRBUF_SIZE];
     /*find a device*/
     device = pcap_lookupdev (error_lookup);
-    if(device == NULL)
+    if(device == NULL) 
     {
         printf("Any default device found: %s\n", error_lookup);
         return 0;
